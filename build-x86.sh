a4b71dab6a91bfc1f56b969d59a182921cd5198d
Michael Kruse <monagui@meinersbur.de>
2011-02-09 15:10:55 +0100

    - Output allocation sizes - autoreconf

--- build-x86.sh
+++ build-x86.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-./configure --with-lapack="-llapack" --enable-mpi --with-mpidimension=4 CC="mpicc" CCFLAGS="-g -O3"
+./configure --with-lapack="-llapack" --enable-mpi --with-mpidimension=1 CC="mpicc" CCFLAGS="-g -O3"
 make benchmark hmc_tm
 
 # mpirun -np 16 hmc_tm -v -f input.inp
