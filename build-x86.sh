2b187cceb1f1bac5bc279666bbb00d7a44ba4c07
Michael Kruse <tmLQCD@meinersbur.de>
2011-02-10 11:10:39 +0100

    - Rerun autoconf 2.67

--- build-x86.sh
+++ build-x86.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
+export ac_cv_func_malloc_0_nonnull=yes
 
-./configure --with-lapack="-llapack" --enable-mpi --with-mpidimension=4 CC="mpicc" CCFLAGS="-g -O3"
+./configure --with-lapack="-llapack" --enable-mpi --with-mpidimension=2 CC="mpicc" CCFLAGS="-g -O3"
 make benchmark hmc_tm
 
 # mpirun -np 16 hmc_tm -v -f input.inp
