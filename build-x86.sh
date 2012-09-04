cc16f8d70d330d45b193d9d0660c0d186744be93
Michael Kruse <michael.kruse@inria.fr>
2012-09-04 17:10:23 +0200

    Repair non-carry version

--- build-x86.sh
+++ build-x86.sh
@@ -17,10 +17,80 @@ make
 
 
 
+
 cd $ROOTPATH
 
-./configure --with-lapack="-llapack" --with-limedir="${ROOTPATH}/lime" --enable-mpi --with-mpidimension=3 --disable-halfspinor CC="mpicc" CFLAGS="-g -O0 -ffast-math -fopenmp -Wall -Wundef -DBGQ=1 -DBGQ_QPX=0 -DBGQ_FIELD_COORDCHECK=0 -I/usr/lib/gcc/x86_64-linux-gnu/4.6/include -I/usr/include/openmpi -DBGQ_PREFETCH_EXPLICIT=0 -DBGQ_PREFETCH_STREAM=0 -DBGQ_PREFETCH_LIST=0 -DMPI=1 -DBGQ_HM_CARRY=1 -DBGQ_REPLACE=0" OPTARGS="-O0 -g eart " SOPTARGS="-O0 -g badfg" --enable-optimize=no LDFLAGS="-lgomp -lblas"
-make -j4 bgqbench benchmark hmc_tm invert
+echo
+echo Configure tmLQCD.....
 
-# mpirun -np 16 hmc_tm -v -f input.inp
+LAPACK=""
+LAPACK="${LAPACK} -llapack"
 
+
+#CFLAGS="-g -O0 -ffast-math -fopenmp -Wall -Wundef -DBGQ=1 -DBGQ_QPX=0 -DBGQ_FIELD_COORDCHECK=0 -I/usr/lib/gcc/x86_64-linux-gnu/4.6/include -I/usr/include/openmpi -DBGQ_PREFETCH_EXPLICIT=0 -DBGQ_PREFETCH_STREAM=0 -DBGQ_PREFETCH_LIST=0 -DMPI=1 -DBGQ_HM_CARRY=1 -DBGQ_REPLACE=0" 
+CFLAGS=""
+CFLAGS="${CFLAGS} -I/usr/lib/gcc/x86_64-linux-gnu/4.6/include"
+CFLAGS="${CFLAGS} -I/usr/include/openmpi"
+CFLAGS="${CFLAGS} -I/usr/include/openmpi"
+CFLAGS="${CFLAGS} -O0"
+CFLAGS="${CFLAGS} -g"
+CFLAGS="${CFLAGS} -ffast-math"
+CFLAGS="${CFLAGS} -fopenmp"
+CFLAGS="${CFLAGS} -Wall"
+CFLAGS="${CFLAGS} -Wundef"
+
+
+CFLAGS="${CFLAGS} -DBGQ=1"
+#CFLAGS="${CFLAGS} -DXLC=1"
+CFLAGS="${CFLAGS} -DNDEBUG=0"
+#CFLAGS="${CFLAGS} -DBGQ_HM_NOKAMUL=1"
+CFLAGS="${CFLAGS} -DBGQ_QPX=0"
+CFLAGS="${CFLAGS} -DBGQ_PREFETCH_EXPLICIT=0"
+CFLAGS="${CFLAGS} -DBGQ_PREFETCH_STREAM=0"
+CFLAGS="${CFLAGS} -DBGQ_PREFETCH_LIST=0"
+CFLAGS="${CFLAGS} -DBGQ_FIELD_COORDCHECK=0"
+CFLAGS="${CFLAGS} -DMPI=1"
+CFLAGS="${CFLAGS} -DBGQ_HM_CARRY=0"
+CFLAGS="${CFLAGS} -DBGQ_REPLACE=0"
+
+
+#LDFLAGS="-lgomp -lblas"
+LDFLAGS=""
+LDFLAGS="${LDFLAGS} -lgomp"
+LDFLAGS="${LDFLAGS} -lblas"
+
+#-./configure --with-lapack="-llapack" --with-limedir="${ROOTPATH}/lime" --enable-mpi --with-mpidimension=3 --disable-halfspinor CC="mpicc" CFLAGS="-g -O0 -ffast-math -fopenmp -Wall -Wundef -DBGQ=1 -DBGQ_QPX=0 -DBGQ_FIELD_COORDCHECK=0 -I/usr/lib/gcc/x86_64-linux-gnu/4.6/include -I/usr/include/openmpi -DBGQ_PREFETCH_EXPLICIT=0 -DBGQ_PREFETCH_STREAM=0 -DBGQ_PREFETCH_LIST=0 -DMPI=1 -DBGQ_HM_CARRY=1 -DBGQ_REPLACE=0" OPTARGS="-O0 -g eart " SOPTARGS="-O0 -g badfg" --enable-optimize=no LDFLAGS="-lgomp -lblas"
+CONFIGURE=""
+#CONFIGURE="${CONFIGURE} --with-alignment=32"
+CONFIGURE="${CONFIGURE} --without-bgldram"
+CONFIGURE="${CONFIGURE} --with-limedir=${ROOTPATH}/lime"
+CONFIGURE="${CONFIGURE} --enable-mpi"
+#CONFIGURE="${CONFIGURE} --enable-qpx"
+CONFIGURE="${CONFIGURE} --with-mpidimension=XYT"
+#CONFIGURE="${CONFIGURE} --enable-omp"
+CONFIGURE="${CONFIGURE} --enable-gaugecopy"
+#CONFIGURE="${CONFIGURE} --enable-halfspinor"
+CONFIGURE="${CONFIGURE} --disable-halfspinor"
+CONFIGURE="${CONFIGURE} --enable-largefile"
+CONFIGURE="${CONFIGURE} --with-lapack="\"'${LAPACK}'\"
+CONFIGURE="${CONFIGURE} CC=mpicc"
+CONFIGURE="${CONFIGURE} CFLAGS="\"'${CFLAGS}'\"
+#CONFIGURE="${CONFIGURE} F77=bgf77"
+CONFIGURE="${CONFIGURE} LDFLAGS="\"'${LDFLAGS}'\"
+#CONFIGURE="${CONFIGURE} FC=bgxlf_r"
+
+CONFIGURE="${CONFIGURE} --enable-optimize=no"
+#CONFIGURE="${CONFIGURE} --with-lemondir=${ROOTPATH}/lemon"
+
+
+
+echo ${CONFIGURE}
+echo
+eval ./decho ${CONFIGURE}
+echo
+eval ./configure ${CONFIGURE}
+
+
+echo
+echo Making tmLQCD
+make -j4 bgqbench benchmark invert hmc_tm
